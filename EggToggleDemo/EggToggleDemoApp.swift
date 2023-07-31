fc0094673c1d043476521f4d17904d4c879011d0
Evgeny Mitko <emitko@data.ai>
2023-07-31 16:33:38 +0200

    Initial Commit

---
+++ EggToggleDemo/EggToggleDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  EggToggleDemoApp.swift
+//  EggToggleDemo
+//
+//  Created by Evgeny Mitko on 31/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct EggToggleDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
